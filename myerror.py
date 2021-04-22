d4632d1e1e7a7060c8f1741c866ef683b46680c2
Ian Joughin <ian@apl.washington.edu>
2021-04-22 16:46:35 -0700

    added myStrip, myerror, velocityError

---
+++ myerror.py
@@ -0,0 +1,11 @@
+import sys
+def myerror(message,myLogger=None):
+    """ print error and exit """
+    print('\n\t\033[1;31m *** ',message,' *** \033[0m\n') 
+    if myLogger != None :
+        myLogger.logError(message)
+    sys.exit()
+
+
+
+
